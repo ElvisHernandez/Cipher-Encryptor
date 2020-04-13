b13c237b38e8231447fd60db3273fe0e4f9a02df
ElvisHernandez <ElvisHernandezTheone@gmail.com>
2020-04-13 15:52:43 -0400

    added validation for user input

--- encryptor.c
+++ encryptor.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main(void) {
-	printf("Encryptor\n");
+int main(int argc, string argv[]) {
+	printf("Number of options passed by user: %i\n", argc - 1);
+	if (argc != 2) {
+		printf("Usage: ./substitution key\n");
+		return 1;
+	}
+	string key = argv[1];
+	int key_length = 0;
+	for (int i = 0; key[i] != '\0'; i++ ) {
+		key_length++;
+	}
+	printf("Number of characters passed in: %i\n", key_length);
+	if (key_length != 26) {
+		printf("Key must contain 26 characters.\n");
+		return 1;
+	}
 }
+
